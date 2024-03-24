f3b874665be0db3690db10a4a985858345e02c69
Maxim Mironov <mironov.develop@gmail.com>
2024-03-24 14:46:57 +0300

    add redirect after login or registration

--- client/src/features/user-auth/ui/auth-user.tsx
+++ client/src/features/user-auth/ui/auth-user.tsx
@@ -1,10 +1,11 @@
+import { z } from "zod";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 import { AuthControll, AuthModal } from "@/entities/auth-modal";
 
 import { cn } from "@/shared/lib/utils";
 import { useMobxStore } from "@/shared/hooks/useMobxStore";
-import { z } from "zod";
 import { RegistrationSchema } from "@/entities/auth-modal/model/schema";
 
 interface AuthUserProps {
@@ -13,12 +14,17 @@ interface AuthUserProps {
 
 export const AuthUser = observer(({ className }: AuthUserProps) => {
     const { auth } = useMobxStore();
+    const navigate = useNavigate();
 
     const onLogin = (values: { email: string; password: string }) =>
         auth.login(values.email, values.password);
+    // Редиректим на страницу пользователя (защищенный роут)
+    navigate("/app");
 
     const onRegistration = (values: z.infer<typeof RegistrationSchema>) =>
         auth.registration(values.email, values.password, values.username ?? "");
+    // Редиректим на страницу пользователя (защищенный роут)
+    navigate("/app");
 
     return (
         <div className={cn("", className)}>
