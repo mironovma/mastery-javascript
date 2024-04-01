a3b90249baffbe2fdd87e1e379e1a200cd730552
Maxim Mironov <mironov.develop@gmail.com>
2024-04-01 13:56:48 +0300

    add app name

--- client/src/widgets/Navbar/ui/Navbar.tsx
+++ client/src/widgets/Navbar/ui/Navbar.tsx
@@ -27,39 +27,58 @@ export const Navbar = memo(({ className }: NavbarProps) => {
     return (
         <menu
             className={cn(
-                "w-full h-14 px-3 bg-sky-700 dark:bg-slate-900 flex justify-between items-center",
+                "w-full h-14 px-3 bg-sky-700 dark:bg-slate-900 flex items-center sticky",
                 className,
             )}
         >
-            <Sheet>
-                <SheetTrigger>
-                    <MenuIcon className="text-white" />
-                </SheetTrigger>
+            <div className="flex-1 flex justify-start">
+                <Sheet>
+                    <SheetTrigger>
+                        <MenuIcon className="text-white" />
+                    </SheetTrigger>
 
-                <SheetContent side="left">
-                    <SheetHeader>
-                        <SheetTitle>{t("APP_NAME")}</SheetTitle>
-                    </SheetHeader>
-                    <div className="py-4">
-                        <ul className="flex flex-col gap-2">
-                            <li>
-                                <SheetClose asChild>
-                                    <Link to="/">{t("Главная")}</Link>
-                                </SheetClose>
-                            </li>
-                            <li>
-                                <SheetClose asChild>
-                                    <Link to="/about">{t("О нас")}</Link>
-                                </SheetClose>
-                            </li>
-                        </ul>
-                    </div>
-                    <SheetFooter className="absolute bottom-6">
-                        <LangSwither />
-                    </SheetFooter>
-                </SheetContent>
-            </Sheet>
-            <div className="flex items-center gap-3">
+                    <SheetContent side="left">
+                        <SheetHeader>
+                            <SheetTitle>
+                                <h1>
+                                    <span className="text-sky-700">
+                                        {t("APP_NAME").slice(0, 2)}
+                                    </span>
+                                    <span>{t("APP_NAME").slice(2)}</span>
+                                </h1>
+                            </SheetTitle>
+                        </SheetHeader>
+                        <div className="py-4">
+                            <ul className="flex flex-col gap-2">
+                                <li>
+                                    <SheetClose asChild>
+                                        <Link to="/">{t("Главная")}</Link>
+                                    </SheetClose>
+                                </li>
+                                <li>
+                                    <SheetClose asChild>
+                                        <Link to="/about">{t("О нас")}</Link>
+                                    </SheetClose>
+                                </li>
+                            </ul>
+                        </div>
+                        <SheetFooter className="absolute bottom-6">
+                            <LangSwither />
+                        </SheetFooter>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
+            <div className="flex-1 flex justify-center">
+                <h1>
+                    <span className="text-sky-700">
+                        {t("APP_NAME").slice(0, 2)}
+                    </span>
+                    <span>{t("APP_NAME").slice(2)}</span>
+                </h1>
+            </div>
+
+            <div className="flex-1 flex justify-end items-center gap-3">
                 <AuthUser />
                 <ThemeSwitcher />
             </div>
