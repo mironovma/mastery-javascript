7fa12c361d1e274ce0fa7080356ca920bdd4652d
Maxim Mironov <mironov.develop@gmail.com>
2024-04-11 11:18:45 +0300

    refactor app page

--- client/src/pages/app-page/ui/app-page.tsx
+++ client/src/pages/app-page/ui/app-page.tsx
@@ -1,13 +1,42 @@
-import { memo } from "react";
+import { memo, useCallback, useState } from "react";
 
-import { LearnSection } from "@/widgets/learn-section";
+import { LearnBlock } from "@/entities/learn-block";
+import { UserSettings } from "@/features/user-settings";
+import { useMobxStore } from "@/shared/hooks/useMobxStore";
+import { observer } from "mobx-react-lite";
 
-const AppPage = () => {
+const AppPage = observer(() => {
+    const { auth, settings, statistic } = useMobxStore();
+
+    const dailyCards = settings.userSettings?.[0].settings.dailyCards;
+    const [dailyCardsToLearn, setDailyCardsToLearn] = useState<
+        number | undefined
+    >(dailyCards);
+
+    const onSaveNewUserSettings = useCallback(() => {
+        settings.setNewUserSettings(auth.user.id, {
+            dailyCards: dailyCardsToLearn!,
+        });
+    }, [dailyCardsToLearn]);
+
+    const onSetNewDailyCardsToLearn = useCallback((amount: number) => {
+        setDailyCardsToLearn(amount);
+    }, []);
+
     return (
-        <div>
-            <LearnSection />
+        <div className="flex flex-col gap-4">
+            <LearnBlock
+                dailyCardsToLearn={dailyCardsToLearn}
+                learnedToday={statistic.statisticToday?.newCards}
+            />
+            <UserSettings
+                dailyCardsToLearn={dailyCardsToLearn}
+                learnedToday={statistic.statisticToday?.newCards}
+                onSetNewDailyCardsToLearn={onSetNewDailyCardsToLearn}
+                onSaveNewUserSettings={onSaveNewUserSettings}
+            />
         </div>
     );
-};
+});
 
 export default memo(AppPage);
--- client/src/widgets/learn-section/index.ts
+++
@@ -1,1 +0,0 @@
-export { LearnSection } from "./ui/learn-section";
