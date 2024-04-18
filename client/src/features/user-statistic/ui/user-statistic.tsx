1f2c355e959088beea2ee4be4989da7de85678bd
Maxim Mironov <mironov.develop@gmail.com>
2024-04-18 13:54:35 +0300

    feat: user statistic

--- client/src/features/user-statistic/ui/user-statistic.tsx
+++ client/src/features/user-statistic/ui/user-statistic.tsx
@@ -36,14 +36,71 @@ export const UserStatistic = ({ className }: UserStatisticProps) => {
 
     const preparedStatistic = prepareStatistics(statistic.statisticAll);
 
+    const newAll = statistic.statisticAll
+        .map((stat) => stat.newCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const repeatedAll = statistic.statisticAll
+        .map((stat) => stat.repeatedCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const learnedAll = statistic.statisticAll
+        .map((stat) => stat.learnedCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const new7days = preparedStatistic
+        .map((stat) => stat.newCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const repeated7days = preparedStatistic
+        .map((stat) => stat.repeatedCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
+    const learned7days = preparedStatistic
+        .map((stat) => stat.learnedCards)
+        .reduce((acc, curr) => acc + curr, 0);
+
     return (
         <div className={className}>
             <SectionMenuHeader>Статистика</SectionMenuHeader>
             <SectionMenuWrapper>
                 <SectionMenuItem className="flex-col items-start border-b-background border-b-2">
                     <BarChart data={preparedStatistic} />
-                    <div>
-                        Сводка (две колонки: всего | за 7 дней (два столбца))
+                    <div className="grid grid-cols-2 gap-x-1 items-center">
+                        <div>
+                            <p className="text-muted-foreground text-xs">
+                                всего
+                            </p>
+                            <div>{newAll ?? 0}</div>
+                            <div>{repeatedAll ?? 0}</div>
+                            <div>{learnedAll ?? 0}</div>
+                        </div>
+                        <div>
+                            <p className="text-muted-foreground text-xs">
+                                7 дней
+                            </p>
+                            <div className="flex items-center gap-6">
+                                <div className="w-5">{new7days ?? 0}</div>
+                                <div className="flex items-center gap-1">
+                                    <div className="w-2 h-2 bg-rose-400 rounded-[1px]" />{" "}
+                                    <p className="text-xs">Новые карточки</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-6">
+                                <div className="w-5">{repeated7days ?? 0}</div>
+                                <div className="flex items-center gap-1">
+                                    <div className="w-2 h-2 bg-yellow-400 rounded-[1px]" />{" "}
+                                    <p className="text-xs">Повторений</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-6">
+                                <div className="w-5">{learned7days ?? 0}</div>
+                                <div className="flex items-center gap-1">
+                                    <div className="w-2 h-2 bg-green-400 rounded-[1px]" />{" "}
+                                    <p className="text-xs">Выученные</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </SectionMenuItem>
             </SectionMenuWrapper>
